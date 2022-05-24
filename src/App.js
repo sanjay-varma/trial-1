7485223b17fa4b4064dcc975776114c50fbc3454
Sanjay Varma <sanjay@billionlives.in>
2022-05-24 21:21:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+var msg = "Hello World!";
+
+function App() {
+  return (
+    <div>
+      <script>alert("rendering app")</script>
+      <strong>{msg}</strong>
+    </div>
+  );
+}
+
+export default App;
