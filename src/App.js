9a04f4e573c797fd4cb953a5480c2caee5e2c885
Sanjay Varma <sanjay@billionlives.in>
2022-05-26 21:14:00 +0530

    update 26/5

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
 import React from "react";
 
-var msg = "Hello World!";
-
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
 
-    this.state = { bg: "red", x: 0 }
-    setTimeout(this.onTimer, 500)
+    this.inp1 = React.createRef();
+    this.inp2 = React.createRef();
+    this.state = { bg: "red", x: 0, y: 0, msg: "Hello World!" }
+    setInterval(this.onTimerX, 500)
+    setInterval(this.onTimerY, 700)
   }
 
-  onTimer = () => {
-    this.setState({ x: this.state.x + 1 })
-    setTimeout(this.onTimer, 500)
+  onTimerX = () => {
+    this.setState({ x: this.state.x + 1 }, () => { })
+  }
+
+  onTimerY = () => {
+    this.setState({ y: this.state.y + 1 })
   }
 
   inputChanged = (e) => {
-    this.setState({ bg: e.target.value })
+    this.setState({ bg: e.target.value, msg: this.inp1.current.value })
   }
 
   buttonClicked = () => {
-    this.setState({ bg: document.getElementById("input2").value, x: 0 })
+    this.setState({ bg: this.inp2.current.value, x: 0, y: 0 })
   }
 
   render() {
     return (
       <div style={{ backgroundColor: this.state.bg, height: 100, widht: 100 }}>
-        <strong>{msg} - {this.state.x}</strong><br />
-        <input onChange={this.inputChanged} /><br />
-        <input id="input2" />
+        <strong>{this.state.msg} - {this.state.x} - {this.state.y}</strong><br />
+        <input ref={this.inp1} onChange={this.inputChanged} /><br />
+        <input ref={this.inp2} />
         <button onClick={this.buttonClicked} > Change </button>
       </div>
     )
